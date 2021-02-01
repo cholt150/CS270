71ca33e74471ac898d2a4a59796a66e5e5d62804
Cory Holt <cholt150@gmail.com>
2021-01-31 16:14:32 -0800

    modified script

--- HW1/script.sh
+++ HW1/script.sh
@@ -1,13 +1,34 @@
+rm -r readelf_logs
+rm *.o
+rm *.out
+
 gcc main.c -c
 gcc quicksort.c -c
 gcc selectionsort.c -c 
 gcc bubblesort.c -c
 gcc main.c
 
-mkdir readelf_logs/
+mkdir readelf_logs
+mkdir readelf_logs/unoptimized
+mkdir readelf_logs/optimized
 
-readelf -a main.o >readelf_logs/elflog_main.txt
-readelf -a bubblesort.o >readelf_logs/elflog_bubble.txt
-readelf -a quicksort.o >readelf_logs/elflog_quick.txt
-readelf -a selectionsort.o >readelf_logs/elflog_sel.txt
-readelf -a a.out >readelf_logs/elflog_exec.txt
+readelf -a main.o >readelf_logs/unoptimized/elflog_main.txt
+readelf -a bubblesort.o >readelf_logs/unoptimized/elflog_bubble.txt
+readelf -a quicksort.o >readelf_logs/unoptimized/elflog_quick.txt
+readelf -a selectionsort.o >readelf_logs/unoptimized/elflog_sel.txt
+readelf -a a.out >readelf_logs/unoptimized/elflog_exec.txt
+
+rm *.o
+rm *.out
+
+gcc main.c -cO
+gcc quicksort.c -cO
+gcc selectionsort.c -cO
+gcc bubblesort.c -cO
+gcc main.c -O
+
+readelf -a main.o >readelf_logs/optimized/elflog_main.txt
+readelf -a bubblesort.o >readelf_logs/optimized/elflog_bubble.txt
+readelf -a quicksort.o >readelf_logs/optimized/elflog_quick.txt
+readelf -a selectionsort.o >readelf_logs/optimized/elflog_sel.txt
+readelf -a a.out >readelf_logs/optimized/elflog_exec.txt
